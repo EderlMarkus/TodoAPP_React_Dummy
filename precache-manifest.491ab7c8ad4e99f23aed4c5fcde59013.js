ba11194556aab2386220dd8363f90503b1768e6e
EderlMarkus <office@edma.at>
2019-06-05 14:45:13 +0200

    Updates

---
+++ precache-manifest.491ab7c8ad4e99f23aed4c5fcde59013.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5ae60b31f8c92e84125ff1892f981e",
+    "url": "/JS_REACT_TodoAPP_Dummy/index.html"
+  },
+  {
+    "revision": "be04ac0a80f16eee7bb3",
+    "url": "/JS_REACT_TodoAPP_Dummy/static/css/main.dd800566.chunk.css"
+  },
+  {
+    "revision": "cf7567ed88728b33b75b",
+    "url": "/JS_REACT_TodoAPP_Dummy/static/js/2.3b5a862a.chunk.js"
+  },
+  {
+    "revision": "be04ac0a80f16eee7bb3",
+    "url": "/JS_REACT_TodoAPP_Dummy/static/js/main.9305c6aa.chunk.js"
+  },
+  {
+    "revision": "47cdefedb4c6f9c3c816",
+    "url": "/JS_REACT_TodoAPP_Dummy/static/js/runtime~main.0c8bda78.js"
+  }
+]);
