5a32b84d4456e28aab181260168a6dda23370c0c
EderlMarkus <office@edma.at>
2019-06-05 18:29:45 +0200

    Updates

--- precache-manifest.afb026aec6dfd758784088a30e428200.js
+++ precache-manifest.afb026aec6dfd758784088a30e428200.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1e193694d8ba4a14aa6a5f294d194b2",
+    "revision": "71584fa3c9becbd82672e0402ccbec65",
     "url": "/JS_REACT_TodoAPP_Dummy/index.html"
   },
   {
-    "revision": "a1fb4bd69d402734cb87",
+    "revision": "71f9b40f2fe2aaa3968e",
     "url": "/JS_REACT_TodoAPP_Dummy/static/css/main.dd800566.chunk.css"
   },
   {
-    "revision": "cf7567ed88728b33b75b",
-    "url": "/JS_REACT_TodoAPP_Dummy/static/js/2.3b5a862a.chunk.js"
+    "revision": "f45c9add48ccb89ff7cf",
+    "url": "/JS_REACT_TodoAPP_Dummy/static/js/2.27e94c57.chunk.js"
   },
   {
-    "revision": "a1fb4bd69d402734cb87",
-    "url": "/JS_REACT_TodoAPP_Dummy/static/js/main.c9d64aa3.chunk.js"
+    "revision": "71f9b40f2fe2aaa3968e",
+    "url": "/JS_REACT_TodoAPP_Dummy/static/js/main.924a050f.chunk.js"
   },
   {
     "revision": "47cdefedb4c6f9c3c816",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JS_REACT_TodoAPP_Dummy/precache-manifest.78f150bfe6c8ad6db328dfebf8fad0b0.js"
+  "/JS_REACT_TodoAPP_Dummy/precache-manifest.afb026aec6dfd758784088a30e428200.js"
 );
 
 self.addEventListener('message', (event) => {
